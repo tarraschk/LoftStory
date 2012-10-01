3621a435acf8fcd6cbcb8d40e1af15e5c0377539
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2012-10-01 16:10:09 +0200

    Ajout de la classe Genre

---
+++ src/Genre.java
@@ -0,0 +1,31 @@
+/**
+ * 1 oct. 2012 by  maxime
+ * LoftStory
+ */
+
+/**
+ * @author maxime
+ *
+ */
+public class Genre {
+
+	/**
+	 * Nom du genre
+	 */
+	protected String nom;
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+}
