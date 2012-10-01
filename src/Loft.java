f8554932e7e6fb0a4deb214fc607b15ac369d4fa
Alexy83 <alexy.roger@eleves.ec-nantes.fr>
2012-10-01 17:33:18 +0200

    Implémentation des méthodes lancertour, retirer, ajouter

--- src/Loft.java
+++ src/Loft.java
@@ -2,20 +2,48 @@ import java.util.LinkedList;
 
 
 public class Loft {
-	
+
 	protected String nom;
 	protected int[] dimensions;
 	protected LinkedList<Aliment> population;
 	protected Aliment[][] cases;
 	protected int limiteNbTours;
-	
+
 	public Loft(String nom, int[] dim, LinkedList<Aliment> pop, int limTours){
 		this.nom = nom;
 		this.dimensions = dim;
 		this.population = pop;
 		this.limiteNbTours = limTours;
 	}
+
+	public void dessiner(){
+
+	}
+
+	public void ajouter(Aliment objet){
+		int idMax = 0;
+		for (Aliment obj : population){
+			if (obj.identifiant>idMax)
+				idMax = obj.identifiant;
+		}
+		objet.identifiant = idMax+1;
+		this.population.add(objet);
+	}
 	
+	public void retirer (int id){
+		for (Aliment obj : population) {
+			if (obj.identifiant == id) {
+				this.population.remove(obj);
+			}
+		}
+	}
+	
+	public void lancerTour(){
+		for (Aliment obj : population) {
+			((Neuneu)obj).vivre();
+			}
+		}
+
 	public String getNom() {
 		return nom;
 	}
@@ -46,6 +74,4 @@ public class Loft {
 	public void setLimiteNbTours(int limiteNbTours) {
 		this.limiteNbTours = limiteNbTours;
 	}
-
-	
 }
